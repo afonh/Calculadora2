c833f69900da2f064bce2e735a75d68d5e02f525
Filma Bem <filmab.emtv2@gmail.com>
2023-08-27 02:32:32 +0100

    Improved comments

--- calculadora2.py
+++ calculadora2.py
@@ -2,37 +2,35 @@ import sys
 import time
 import math
 
-# Here I got the modules to start my calculator
+# Get the operation and the numbers needed
+
 def calculadora ():
     number_1 = int(input("primeiro número: "))
     number_2 = int(input("segundo número: "))
-    # Here i decided the variable as "primeiro número" and "segundo número" and the function for below
     print("Escolhe a operação entre soma, diferença, multiplicação, divisão, potencialização e raíz quadrada")
     operation = input("Qual operação: ") .lower ()
     operacoes (operation=operation , number_1=number_1 , number_2=number_2)
-    # Here put the operacao as a variable for "qual operação" and .lower to cut down the chance of someone messing with the script  and to get the "connector between this function and the function below"
+
+# Get the operations and deal with the numbers
 
 def operacoes (operation , number_1 , number_2):
     if operation == 'soma':
         soma = number_1 + number_2
         mensagem = "A soma de {} + {} = {}" .format(number_1 , number_2, soma)
         print(soma)
-        # Here i got the start of the function with the information shared by the last code of the first function and the plus calculator
 
     elif operation == 'diferença':
         diferença = number_1 - number_2
         mensagem = "A diferença de {} - {} = {}" .format(number_1 , number_2 , diferença)
         print(diferença)
-# The minus calculator
     elif operation == 'multiplicação':
         multiplicação = number_1 * number_2
         mensagem ="O produto de {} x {} = {}"  .format(number_1 , number_2 , multiplicação)
-# The multiplication calculator
+
 
     elif operation == 'divisão':
         divisão = number_1 / number_2
         mensagem = "O quociente de {} / {} = {}" .format(number_1 , number_2 , divisão)
-# The division calculator
     elif operation == 'potencialização':
         potencialização = number_1 ** number_2
         mensagem = "{} elevado a {} = {}"    .format(number_1 , number_2 , potencialização)
@@ -43,17 +41,24 @@ def operacoes (operation , number_1 , number_2):
         messagem = "A raíz quadrada de {} é {}".format(number_1, square_root)
     
     print(mensagem)
+
+    # Ask user if he wants to do another calculation
+
     another_operation = input("quer fazer outra operação? ")
     if another_operation.lower() == 'sim':
         calculadora()
     elif another_operation.lower() == 'não':
         sys.exit()
+
+    # Just here to deal with exceptions
     else: 
         print("Ou sim ou não")
         time.sleep(30)
         sys.exit()
+
+
 calculadora()
-# Here is the end with print(message) showing the results and the setup to start another calculation
+
     
     
 
